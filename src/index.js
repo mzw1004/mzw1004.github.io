671b6d19308e8af2359344343ff3cebf9f5671ab
Zhiwei.Ma <mazhiwei1004@gmail.com>
2021-09-10 00:45:08 +0800

    feature: test

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ function component() {
     const element = document.createElement('div');
 
     // lodash 在当前 script 中使用 import 引入
-    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+    element.innerHTML = _.join(['Hello!'], ' ');
 
     return element;
 }
