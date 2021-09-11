4545a2d25e4319b06d0b26b388048632266e9360
Zhiwei.Ma <mazhiwei1004@gmail.com>
2021-09-11 22:06:05 +0800

    improve: add effect.

--- src/index.js
+++ src/index.js
@@ -1,12 +1,38 @@
-import _ from 'lodash';
+import anime from 'animejs'
 
-function component() {
-    const element = document.createElement('div');
+anime.timeline({
+	loop: true
+})
+.add({
+	targets: '.hello span',
+	translateY: -10,
+	easing: 'easeInOutQuad',
+	loop: true,
+	delay: (el, i) => i * 100,
+})
+.add({
+	targets: '.hello span',
+	translateY: 0,
+	easing: 'easeInOutQuad',
+	loop: true,
+	delay: (el, i) => i * 100,
+});
 
-    // lodash 在当前 script 中使用 import 引入
-    element.innerHTML = _.join(['Hello!'], ' ');
-
-    return element;
+for (let node of document.querySelectorAll('h2 a')) {
+	node.addEventListener('mouseenter', evt => {
+		anime.remove(evt.target);
+		anime({
+		  targets: evt.target,
+		  scale: 1.1,
+		  duration: 500,
+		});
+	});
+	node.addEventListener('mouseleave', evt => {
+		anime.remove(evt.target);
+		anime({
+		  targets: evt.target,
+		  scale: 1.0,
+		  duration: 500,
+		});
+	});
 }
-
-document.body.appendChild(component());
