2b167f408bbddc8009c26d308dd44ec2cf36f016
Zhiwei.Ma <mazhiwei1004@gmail.com>
2021-09-13 22:40:21 +0800

    improve: add favicon.

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,9 @@
 const path = require('path');
 
 module.exports = {
-  entry: './src/index.js',
+  entry: './src/js/index.js',
   output: {
-    filename: 'main.js',
+    filename: 'main.bundle.js',
     path: path.resolve(__dirname, 'dist'),
   },
 };
